fd859ce960f267be507c430ead2c98f5a30d2e37
neilthaker07 <neilthaker07@gmail.com>
2017-04-12 20:00:11 -0700

    Correction in getting code from users repo.

--- agent_deployer.sh
+++ agent_deployer.sh
@@ -1,13 +1,23 @@
 git_url="$1"
 echo $git_url
 
-git clone $git_url
-
 SUBSTRING1=$(echo $git_url| cut -d'/' -f 5)
 
 repo_name=$(echo $SUBSTRING1| cut -d'.' -f 1)
 
-cd $repo_name
+for folder in */ ; do
+    echo "$folder"
+done
+
+foldername=$(echo $folder| cut -d'/' -f 1)
+
+if [ "$foldername" == "$repo_name" ]; then
+	cd $repo_name 
+	git pull
+else
+	git clone $git_url
+	cd $repo_name
+fi
 
 while read install_value; do
 	pip install $install_value
