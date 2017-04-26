1944ea48eff2c4bf0d1e458489a1a2f97c3f2d0a
Nishant Rathi <nishantrathi@Nishants-MacBook-Pro.local>
2017-04-26 15:49:00 -0700

    added second component

---
+++ ui/src/app/project-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  selector: 'project-table',
+  templateUrl: './project-table.component.html'
+
+})
+export class ProjectTableComponent {
+}
