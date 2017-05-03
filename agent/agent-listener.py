28ce044bcc1ccb45d011c6bbc986e3453f6afd66
Xiaoyu (Laura) <bhakti.shah@sjsu.edu>
2017-05-03 16:48:21 -0700

    Pass url to sh

--- agent/agent-listener.py
+++ agent/agent-listener.py
@@ -1,22 +1,29 @@
 import paho.mqtt.client as mqtt
+import subprocess
+import shlex
+import os
 
+TOPIC='https://github.com/rashmishrm/sample-repo'
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("$SYS/broker/https://github.com/rashmishrm/sample-repo")
+    client.subscribe("$SYS/broker/"+TOPIC)
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
+    os.system('sh agent_deployer.sh '+TOPIC)
+    
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
 client.connect("iot.eclipse.org", 1883, 60)
+
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
