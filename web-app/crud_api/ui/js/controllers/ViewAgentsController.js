a0e0e9d81f178d23686d21d84eb10104fa1eef43
nrats <nishantrathi100@gmail.com>
2017-05-10 14:42:12 -0700

    dynamically getting agent status

--- web-app/crud_api/ui/js/controllers/ViewAgentsController.js
+++ web-app/crud_api/ui/js/controllers/ViewAgentsController.js
@@ -1,12 +1,13 @@
 angular.module('AppDeployerDashboard')
-.controller('ViewAgentsController', function($scope, ProjectService, $routeParams , $location) {
+.controller('ViewAgentsController', function($rootScope, $scope, ProjectService, $routeParams , $location,$interval) {
 
 $scope.loadAgents = function() {
   ProjectService.getAgents($routeParams.projectId).then(function (response) {
     $scope.agentsList = response.data;
   });
 };
-stop = $interval(function() {
+
+$interval(function() {
             $scope.loadAgents();
           }, 1000);
 
